dd6023f1c79533765277dd38aa920dc60432f7ef
Sam Willis <samwilliswork@gmail.com>
2019-02-20 10:55:30 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    get 'restaurants/index'
+  end
+  resources :restaurants do
+    collection do #restaurants
+      get "top", to: "restaurants#top"
+    end
+    member do #restaurants/:id
+      get "chef", to: "restaurants#chef"
+    end
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [ :show, :edit, :update, :destroy ]
+  namespace :admin do
+    resources :restaurants, only: [:index] #admin/restaurants
+  end
 end
